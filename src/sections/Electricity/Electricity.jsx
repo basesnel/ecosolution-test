11d3a6667b53b59d883c55444a4ec3a111d3a658
basesnel <basesnel@gmail.com>
2023-11-30 17:19:26 +0200

    Added media-styles for Electricity-title.

--- src/sections/Electricity/Electricity.jsx
+++ src/sections/Electricity/Electricity.jsx
@@ -1,9 +1,11 @@
 import Container from 'components/Container';
 import Title from 'components/Title';
 
+import css from './Electricity.module.css';
+
 export default function Electricity() {
   return (
-    <section>
+    <section className={css.electricity}>
       <Container>
         <Title level={2} caption="electricity we produced for all time" />
         <span>1</span>
