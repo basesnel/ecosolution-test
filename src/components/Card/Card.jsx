f40ceee14188e5c5d63c7c4acc4cf103cad92827
basesnel <basesnel@gmail.com>
2023-11-30 14:47:09 +0200

    Added Values section.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,24 @@
 import PropTypes from 'prop-types';
 
+import icons from '../../assets/images/icons.svg';
+
 import css from './Card.module.css';
 
-export default function Card({ children }) {
-  return <div className={css.card}>{children}</div>;
+export default function Card({ title, icon, children }) {
+  return (
+    <div className={css.card}>
+      <div className={css['card-title']}>
+        <svg className={css.icon}>
+          <use href={`${icons}#${icon}`}></use>
+        </svg>
+        {title}
+      </div>
+      {children}
+    </div>
+  );
 }
 
 Card.propTypes = {
+  title: PropTypes.node.isRequired,
   children: PropTypes.any.isRequired,
 };
--- src/sections/Values/Values.jsx
+++ src/sections/Values/Values.jsx
@@ -4,38 +4,49 @@ import Card from 'components/Card';
 import Text from 'components/Text';
 import Title from 'components/Title';
 
+import css from './Values.module.css';
+
 export default function Values() {
   return (
     <section>
       <Container>
-        <Title level={2} caption="main values of our company" />
-        <Text>
-          EcoSolution envisions a world where sustainable energy solutions power
-          a brighter and cleaner future for all. We aspire to be at the
-          forefront of the global shift towards renewable energy, leading the
-          way in innovative technologies that harness the power of nature to
-          meet the world's energy needs.
-        </Text>
+        <div role="presentation" className={css['title-wrap']}>
+          <Title level={2} caption="main values of our company" />
+          <div role="presentation" className={css['text-wrap']}>
+            <Text>
+              EcoSolution envisions a world where sustainable energy solutions
+              power a brighter and cleaner future for all. We aspire to be at
+              the forefront of the global shift towards renewable energy,
+              leading the way in innovative technologies that harness the power
+              of nature to meet the world's energy needs.
+            </Text>
+          </div>
+        </div>
         <Grid>
-          <Card>
-            <Title level={3} caption="openness" />
+          <Card
+            title={<Title level={3} caption="openness" />}
+            icon="maximize-circle"
+          >
             <Text>to the world, people, new ideas and projects</Text>
           </Card>
-          <Card>
-            <Title level={3} caption="responsibility" />
+          <Card
+            title={<Title level={3} caption="responsibility" />}
+            icon="global-edit"
+          >
             <Text>
               we are aware that the results of our work have an impact on our
               lives and the lives of future generations
             </Text>
           </Card>
-          <Card>
-            <Title level={3} caption="innovation" />
+          <Card
+            title={<Title level={3} caption="innovation" />}
+            icon="cpu-charge"
+          >
             <Text>
               we use the latest technology to implement non-standard solutions
             </Text>
           </Card>
-          <Card>
-            <Title level={3} caption="quality" />
+          <Card title={<Title level={3} caption="quality" />} icon="ranking">
             <Text>
               we do not strive to be the first among others, but we want to be
               the best in our business
