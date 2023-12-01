8b0a775324c0caf9150540fdf469c01297baaa70
basesnel <basesnel@gmail.com>
2023-12-01 23:53:48 +0200

    Changed index / length output for slider.

--- src/sections/Cases/Cases.jsx
+++ src/sections/Cases/Cases.jsx
@@ -1,30 +1,67 @@
+import { useState } from 'react';
+import { flushSync } from 'react-dom';
+
 import Container from 'components/Container';
 import RoundButton from 'components/RoundButton';
 import Slide from 'components/Slide';
 import Title from 'components/Title';
 
 import { slides } from 'constants';
-import { useState } from 'react';
 
 import css from './Cases.module.css';
 
 export default function Cases() {
-  const [position, setPosition] = useState(0);
+  const [index, setIndex] = useState(0);
+
+  const formattedNum = num => {
+    return num > 9 ? num : `0${num}`;
+  };
 
   return (
     <section>
       <Container>
         <Title level={2} caption="successful cases of our company" />
         <div className={css.control}>
-          <span className={css.position}>{`0${position + 1} /0${
-            slides.length
-          }`}</span>
+          <div>
+            <span className={css.position}>{formattedNum(index + 1)}</span>
+            <span className={css.length}>
+              {` /${formattedNum(slides.length)}`}
+            </span>
+          </div>
           <div className={css.switches}>
             <RoundButton
               direction="left"
-              onClick={() => setPosition(p => p - 1)}
+              onClick={() => {
+                flushSync(() => {
+                  if (index > 0) {
+                    setIndex(index - 1);
+                  } else {
+                    setIndex(slides.length - 1);
+                  }
+                });
+                // selectedRef.current.scrollIntoView({
+                //   behavior: 'smooth',
+                //   block: 'nearest',
+                //   inline: 'center',
+                // });
+              }}
             />
-            <RoundButton onClick={() => setPosition(p => p + 1)} />
+            <RoundButton
+              onClick={() => {
+                flushSync(() => {
+                  if (index < slides.length - 1) {
+                    setIndex(index + 1);
+                  } else {
+                    setIndex(0);
+                  }
+                });
+                // selectedRef.current.scrollIntoView({
+                //   behavior: 'smooth',
+                //   block: 'nearest',
+                //   inline: 'center',
+                // });
+              }}
+            />
           </div>
         </div>
         {slides.map(({ image, desc, place, enterprise, date }) => (
