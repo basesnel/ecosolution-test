10e40501aad9fd42b427f6f1a44289c1234dcda1
basesnel <basesnel@gmail.com>
2023-12-02 21:07:06 +0200

    Added interactive background to header on scroll.

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Container from 'components/Container';
 import MainNav from 'components/MainNav';
 import Logo from 'components/Logo';
@@ -7,8 +9,24 @@ import css from './Header.module.css';
 import ContactUsLink from 'components/ContactUsLink';
 
 export default function Header() {
+  const [header, setHeader] = useState(`${css.header}`);
+
+  const listenerScrollEvent = e => {
+    if (window.scrollY < 105) {
+      return setHeader(`${css.header}`);
+    } else {
+      return setHeader(`${css.header} ${css.filled}`);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenerScrollEvent);
+
+    return () => window.removeEventListener('scroll', listenerScrollEvent);
+  }, []);
+
   return (
-    <header className={css.header}>
+    <header className={header}>
       <Container variant="nav-bar">
         <Logo subLength={5} />
         <MainNav>
