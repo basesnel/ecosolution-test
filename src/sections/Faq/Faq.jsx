71b64c0cd51f7d942625728156c50acde44ca263
basesnel <basesnel@gmail.com>
2023-12-03 15:22:44 +0200

    Styled media question.

--- src/sections/Faq/Faq.jsx
+++ src/sections/Faq/Faq.jsx
@@ -1,15 +1,24 @@
 import Container from 'components/Container';
 import Title from 'components/Title';
+import Text from 'components/Text';
 import Section from 'components/Section';
 import Questions from 'components/Questions';
+import ContactUsLink from 'components/ContactUsLink';
+
+import css from './Faq.module.css';
 
 export default function Faq() {
   return (
     <Section part="faq">
       <Container>
         <Title level={2} caption="frequently asked questions" />
-        <Questions />
-        <button>Contact Us</button>
+        <div role="presentation" className={css['content-wrap']}>
+          <Questions />
+          <div className={css['contact-us']}>
+            <Text>Didn&apos;t find the answer to your question?</Text>
+            <ContactUsLink caption="Contact Us" />
+          </div>
+        </div>
       </Container>
     </Section>
   );
