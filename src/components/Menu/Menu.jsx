c2b0fd0fa6d0fbde33b48ffff5703c8d0d47f60f
basesnel <basesnel@gmail.com>
2023-12-19 20:55:33 +0200

    aria-accessibility practic

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -10,39 +10,46 @@ export default function Menu({ onClose, onHide }) {
   return (
     <div className={css.backdrop} onClick={onHide}>
       <div className={css.menu}>
-        <button className={css['menu-close']} onClick={onClose}>
-          <Icon
-            icon="menu-close"
-            width={20}
-            height={20}
-            label="Close the menu"
-          />
+        <button
+          className={css['menu-close']}
+          onClick={onClose}
+          aeia-label="Close the menu"
+        >
+          <Icon icon="menu-close" width={20} height={20} />
           Close
         </button>
         <ul className={css['menu-list']}>
           {menu.map(({ id, item }) => (
             <li key={id} className={css['menu-item']}>
-              <a href={`#${id}`} className={css['menu-link']} onClick={onClose}>
+              <a
+                href={`#${id}`}
+                className={css['menu-link']}
+                onClick={onClose}
+                aria-label={`Go to section ${item}`}
+              >
                 {item}
-                <Icon
-                  icon="menu-arrow"
-                  width={16}
-                  height={16}
-                  label={`Go to section ${item}`}
-                />
+                <Icon icon="menu-arrow" width={16} height={16} />
               </a>
             </li>
           ))}
         </ul>
         <ul className={css['menu-socnet']}>
           <li className={css['item-socnet']}>
-            <a className={css['link-socnet']} href="https://www.facebook.com">
-              <Icon icon="menu-facebook" />
+            <a
+              className={css['link-socnet']}
+              href="https://www.facebook.com"
+              aria-label="Go to Facebook page"
+            >
+              <Icon icon="menu-facebook" width={24} height={24} />
             </a>
           </li>
           <li className={css['item-socnet']}>
-            <a className={css['link-socnet']} href="https://www.instagram.com">
-              <Icon icon="menu-instagram" />
+            <a
+              className={css['link-socnet']}
+              href="https://www.instagram.com"
+              aria-label="Go to Instagram page"
+            >
+              <Icon icon="menu-instagram" width={24} height={24} />
             </a>
           </li>
         </ul>
