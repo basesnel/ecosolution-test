0dd4b6585c786ee086c298710865e2ce3e934a2d
basesnel <basesnel@gmail.com>
2024-06-12 20:34:25 +0300

    Added index.js to components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,59 @@
 import Button from './Button';
+import Card from './Card';
+import CardImage from './CardImage';
+import Contacts from './Contacts';
+import ContactUsLink from './ContactUsLink';
+import Container from './Container';
+import Copyright from './Copyright';
+import Counter from './Counter';
+import FitContent from './FitContent';
+import FooterComponent from './FooterComponent';
+import Form from './Form';
+import Grid from './Grid';
+import HeroImage from './HeroImage';
 import Icon from './Icon';
+import Input from './Input';
+import Logo from './Logo';
+import MainNav from './MainNav';
+import Menu from './Menu';
+import MenuButton from './MenuButton';
+import Questions from './Questions';
+import ResponsiveImage from './ResponsiveImage';
+import RoundButton from './RoundButton';
+import Section from './Section';
+import Slide from './Slide';
+import Slider from './Slider';
+import Text from './Text';
+import TextArea from './TextArea';
+import Title from './Title';
 
-export { Button, Icon };
+export {
+  Button,
+  Card,
+  CardImage,
+  Contacts,
+  ContactUsLink,
+  Container,
+  Copyright,
+  Counter,
+  FitContent,
+  FooterComponent,
+  Form,
+  Grid,
+  HeroImage,
+  Icon,
+  Input,
+  Logo,
+  MainNav,
+  Menu,
+  MenuButton,
+  Questions,
+  ResponsiveImage,
+  RoundButton,
+  Section,
+  Slide,
+  Slider,
+  Text,
+  TextArea,
+  Title,
+};
